40dcaaae2111d07856825ed08e2f2023897e30b9
Emmanuel Mack <mack.reboisson@gmail.com>
2022-02-16 08:56:17 +0100

    jgjfjj

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  #edit : En tant qu’utilisateur, je peux éditer une tâche (la marquer comme terminée / mettre à jour le titre et les détails)
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'task/:id', to: 'task#update'
+  # index
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # En tant qu’utilisateur, je peux supprimer une tâche
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
